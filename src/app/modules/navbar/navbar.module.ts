bd7f16f57ac78a87677251fe99b751d932b088fb
Joseba <joseba_andres@hotmail.com>
2020-11-02 20:56:32 +0100

    addiding menu

--- src/app/modules/navbar/navbar.module.ts
+++ src/app/modules/navbar/navbar.module.ts
@@ -5,10 +5,20 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { CustomIconsModule } from '../custom-icons/custom-icons.module';
 import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [NavbarComponent],
-  imports: [CommonModule, MatToolbarModule, MatIconModule, CustomIconsModule, RouterModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    CustomIconsModule,
+    RouterModule,
+    FlexLayoutModule,
+    MatButtonModule,
+  ],
   exports: [NavbarComponent],
 })
 export class NavbarModule {}
