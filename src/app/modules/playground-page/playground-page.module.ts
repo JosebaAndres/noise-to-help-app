22a565cbb00c905130e7262e3776c27e9740d2ff
Joseba <joseba_andres@hotmail.com>
2020-11-18 00:17:43 +0100

    add cards in the playground

--- src/app/modules/playground-page/playground-page.module.ts
+++ src/app/modules/playground-page/playground-page.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ViewModule } from '../view/view.module';
 import { PlaygroundPageComponent } from './playground-page.component';
 import { PlaygroundPageRoutingModule } from './playground-page-routing.module';
+import { CardModule } from '../card/card.module';
+import { BigCardModule } from '../big-card/big-card.module';
 
 @NgModule({
   declarations: [PlaygroundPageComponent],
-  imports: [CommonModule, PlaygroundPageRoutingModule, ViewModule],
+  imports: [CommonModule, PlaygroundPageRoutingModule, ViewModule, CardModule, BigCardModule],
   exports: [PlaygroundPageComponent],
 })
 export class PlaygroundPageModule {}
