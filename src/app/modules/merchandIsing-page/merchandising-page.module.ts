dfb64a7dc422a8119a1d88cdc0a419471bc602b5
Joseba <joseba_andres@hotmail.com>
2020-11-24 18:36:05 +0100

    Add merchandising page

--- src/app/modules/merchandIsing-page/merchandising-page.module.ts
+++ src/app/modules/merchandIsing-page/merchandising-page.module.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ViewModule } from '../view/view.module';
 import { MerchandisingPageComponent } from './merchandising-page.component';
 import { MerchandisingPageRoutingModule } from './merchandising-page-routing.module';
-import { UnderConstructionModule } from '../under-construction/under-construction.module';
 
 @NgModule({
   declarations: [MerchandisingPageComponent],
-  imports: [CommonModule, MerchandisingPageRoutingModule, ViewModule, UnderConstructionModule],
+  imports: [CommonModule, MerchandisingPageRoutingModule, ViewModule],
   exports: [MerchandisingPageComponent],
 })
 export class MerchandisingUsPageModule {}
